e3b510f1ff08aa0d742d0600b256451f8defcb88
abr-ya <gamer05@list.ru>
2023-10-08 10:59:49 +0300

    feat: Create main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Footer, Navbar } from "./components";
+import { Benefits, Classes, Contact, Home } from "./sections";
+
 const App = () => {
-  return <div className="bg-gray-20 text-primary-500 flex p-4	text-xl shadow-md">Hello, React + Tailwind!</div>;
+  return (
+    <div className="app bg-gray-20">
+      <Navbar />
+      <Home />
+      <Benefits />
+      <Classes />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/sections/index.ts
@@ -0,0 +1,6 @@
+import Benefits from "./Benefits/Benefits";
+import Classes from "./Classes/Classes";
+import Contact from "./Contact/Contact";
+import Home from "./Home/Home";
+
+export { Benefits, Classes, Contact, Home };
